965df56de79fab18021507bd828eb474985bad54
Álvaro <ainiguezdislaa@gmail.com>
2023-02-27 11:14:10 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,9 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.List;
+import dominio.ej14_9;
+public class Principal {
+    public static void main(String[] args) {
+        List<String> al = List.of("Hola"," ","mundo"," ","cruel");
+        System.out.println(ej14_9.concatenarCadenas(al));
+    }
 }
--- src/dominio/ej14_9.java
+++ src/dominio/ej14_9.java
@@ -1,2 +1,9 @@
-package dominio;public class ej14_9 {
+package dominio;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+public class ej14_9 {
+    public static String concatenarCadenas(List<String> al){
+        return Stream.iterate(0,i->i+1).limit(al.size()).map(i->al.get(i)).reduce("",(a,b)->a+b);
+    }
 }
